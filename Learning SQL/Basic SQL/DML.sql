1c43d7e4ab22c56000568a3780da948e9d78728a
RevazRevazashvili <revazi.revazashvili380@ens.tsu.edu.ge>
2025-05-29 16:07:34 +0400

    added DML.sql file

---
+++ Learning SQL/Basic SQL/DML.sql
@@ -0,0 +1,13 @@
+use MySalesDB
+
+-- inserting values to the table customers
+INSERT INTO dbo.customers(customerName, customersurname, age, salary) 
+VALUES('John', 'Herbert', 70, 1349);
+INSERT INTO dbo.customers(customerName, customersurname, age, salary) 
+VALUES('Peter', 'Griffin', 45, 2200);
+INSERT INTO dbo.customers(customerName, customersurname, age, salary) 
+VALUES('Clevelend', 'Brown', 42, 1200);
+INSERT INTO dbo.customers(customerName, customersurname, age, salary) 
+VALUES('Glen', 'Quagmire', 61, 2000);
+INSERT INTO dbo.customers(customerName, customersurname, age, salary) 
+VALUES('Joe', 'Swanson', 40, 3000);
